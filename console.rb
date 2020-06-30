55d0e30879abae4a0ab66c5674f57dd46de7c735
Stephen <stephen_h369@live.co.uk>
2020-06-30 15:04:08 +0100

    update method works

--- console.rb
+++ console.rb
@@ -2,3 +2,5 @@ require_relative('models/PropertyTracker')
 
 property1 = PropertyTracker.new({'address' => '123 sesame street', 'value' => 1000000, 'number_of_bedrooms' => 10, 'build' => 'house'})
 property1.save()
+property1.value = 3000000
+property1.update
--- models/PropertyTracker.rb
+++ models/PropertyTracker.rb
@@ -23,5 +23,14 @@ class PropertyTracker
         db.close()
     end
 
+    def update()
+        db = PG.connect ({dbname: 'property_tracker', host: 'localhost' })
+        sql = "UPDATE property_tracker SET (address, value, number_of_bedrooms, build) = ($1, $2, $3, $4) WHERE id = $5"
+        values = [@address, @value, @number_of_bedrooms, @build, @id]
+        db.prepare('update', sql)
+        db.exec_prepared('update', values)
+        db.close()
+    end
+
 
 end
